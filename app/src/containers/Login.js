ff8c78cdc50560508d6d1b8e50a8ec695e8ae743
LeiWang <pvtevan@outlook.com>
2019-04-19 18:05:00 +0800

    微调

--- app/src/containers/Login.js
+++ app/src/containers/Login.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import { Row, Col, Input, Button } from 'antd'
+
 class LoginContainer extends Component {
-  constructor (props) {
-    super(props)
-  }
   render () {
-    return <div>LoginContainer</div>
+    return (
+      <div>
+        <Row>
+          <Col xs={2} sm={4} md={6} lg={8} xl={10} />
+          <Col xs={20} sm={16} md={12} lg={8} xl={4}>
+            Col范德萨发烦烦烦烦烦烦烦烦烦烦烦烦烦烦烦
+          </Col>
+          <Col xs={2} sm={4} md={6} lg={8} xl={10} />
+        </Row>
+        {/* <Input>a</Input>
+        <Input>b</Input>
+        <Button>a</Button>
+        <Button>a</Button> */}
+      </div>
+    )
   }
 }
 export default LoginContainer
