8c6c8ad7198218fd1c6e52061f853fc862a85425
LeiWang <pvtevan@outlook.com>
2019-04-23 20:39:20 +0800

    调整

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -14,14 +14,13 @@ class LoginContainer extends Component {
         <Row>
           <Col xs={2} sm={4} md={6} lg={8} xl={10} />
           <Col xs={20} sm={16} md={12} lg={8} xl={4}>
-            Col范德萨发烦烦烦烦烦烦烦烦烦烦烦烦烦烦烦
+            <div style={{ backgroundColor: 'blue' }}>
+              <Button>a</Button>
+              <Button>a</Button>
+            </div>
           </Col>
           <Col xs={2} sm={4} md={6} lg={8} xl={10} />
         </Row>
-        {/* <Input>a</Input>
-        <Input>b</Input>
-        <Button>a</Button>
-        <Button>a</Button> */}
       </div>
     )
   }
