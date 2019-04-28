b6a9d1ca06ad36a5bf26658204461e59d1d654a6
LeiWang <pvtevan@outlook.com>
2019-04-28 17:38:25 +0800

    添加saga

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -2,10 +2,15 @@
 import { createBrowserHistory } from 'history'
 
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import { routerMiddleware } from 'connected-react-router'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
+
 import createRootReducer from '../reducer/rootReducer'
+import mySaga from '../sagas'
+
+const sagaMiddleware = createSagaMiddleware()
 
 export const history = createBrowserHistory()
 
@@ -13,8 +18,11 @@ export default function configureStore (preloadedState = {}) {
   const store = createStore(
     createRootReducer(history), // root reducer with router state
     preloadedState,
-    composeWithDevTools(applyMiddleware(logger, routerMiddleware(history)))
+    composeWithDevTools(
+      applyMiddleware(logger, routerMiddleware(history), sagaMiddleware)
+    )
   )
+  sagaMiddleware.run(mySaga)
 
   return store
 }
