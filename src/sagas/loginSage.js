2ec00f7d204a5ef11a280c8cef5d8f668c8be099
LeiWang <pvtevan@outlook.com>
2019-04-29 13:11:15 +0800

    修复问题

--- src/sagas/loginSage.js
+++ src/sagas/loginSage.js
@@ -1,14 +1,14 @@
-import { call, put, takeEvery, take, all, fork } from 'redux-saga/effects'
+import { call, put, takeEvery, all, fork } from 'redux-saga/effects'
 import { LOGIN } from '../actions/login.action'
 import Api from '../api/index'
 
 function * fetchData (action) {
   try {
-    console.log(Api)
-    const data = yield call(Api.login(action.payload))
+    const data = yield call(Api.login, action.payload)
 
     yield put({ type: 'FETCH_SUCCEEDED', data })
   } catch (error) {
+    console.table(error)
     yield put({ type: 'FETCH_FAILED', error })
   }
 }
