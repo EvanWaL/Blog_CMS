7dd7e4e304c184e21e2c527b87a2c338f59c6e83
LeiWang <pvtevan@outlook.com>
2019-05-09 14:20:06 +0800

    添加404图片

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+import notFound from '../assets/image/404.jpg'
+
 class NotFound extends Component {
   render () {
-    return <div>404</div>
+    return (
+      <div>
+        <img src={notFound} style={{ width: '100vw', height: '100vh' }} />
+      </div>
+    )
   }
 }
 export default NotFound
